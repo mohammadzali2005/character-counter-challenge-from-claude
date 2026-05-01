585010b0209b53e846bb990c8d7d86f74f7c52d3
mohammadzali <mzking1384@gmail.com>
2026-05-01 11:39:48 +0300

    created styles.css and script.css and connected them to index.html. started the default settings of styles.css and wrote the challenge in index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Chalenge: 
+
+Title: Character counter
+=> Build a simple webpage with a text area where the user can type. 
+As they type, show a live character count below it. Warn the user when they're getting close to a limit, and stop them from going over it.
+
+Requirements: 
+=> A <textarea> where the user can type A counter below it showing how many characters have been typed, e.g. 32 / 150
+The counter turns orange when the user is within 20 characters of the limit
+The counter turns red and the user cannot type any more once the limit (150) is reached
+
+*/
