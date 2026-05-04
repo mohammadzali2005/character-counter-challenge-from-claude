a70f1b2a692a4b5032a8fc6757f2169e46d828e4
mohammadzali <mzking1384@gmail.com>
2026-05-04 19:32:59 +0300

    Done

--- script.js
+++ script.js
@@ -5,8 +5,39 @@ Title: Character counter
 As they type, show a live character count below it. Warn the user when they're getting close to a limit, and stop them from going over it.
 
 Requirements: 
-=> A <textarea> where the user can type A counter below it showing how many characters have been typed, e.g. 32 / 150
+=> A <textarea> where the user can type and a counter below it showing how many characters have been typed, e.g. 32 / 150
 The counter turns orange when the user is within 20 characters of the limit
 The counter turns red and the user cannot type any more once the limit (150) is reached
 
 */
+
+const textarea = document.getElementById('textarea-el');
+const text = document.getElementById('text-el');
+const note = document.getElementById('note');
+
+textarea.addEventListener('input', () => {
+    let count = textarea.value.length;
+
+    if (count >= 130 && count < 150){
+        textarea.style.border = "2px solid orangered";
+        text.style.color = "orangered";
+        textarea.style.boxShadow = "0 0 20px orangered";
+
+        note.textContent = "Be Careful";
+        note.style.color = "orangered";
+    } else if (count === 150){
+        textarea.style.borderColor = "darkred";
+        text.style.color = "darkred";
+        textarea.style.boxShadow = "0 0 20px darkred";
+
+        note.textContent = "Maximum";
+        note.style.color = "darkred";
+    } else {
+        textarea.style.border = "2px solid transparent";
+        text.style.color = "black";
+        textarea.style.boxShadow = "0 0 20px gray";
+
+        note.textContent = "";
+    }
+    text.textContent = `${count} / 150`;
+})
